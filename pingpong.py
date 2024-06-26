a40f08a48a30facd4f6a2c749257f8dcd7454685
rnntbgnifi <jergus.betak@gmail.com>
2024-06-26 16:23:07 +0100

    Add files via upload

---
+++ pingpong.py
@@ -0,0 +1,15 @@
+from pygame import *
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+while game:
+    pass
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    display.update()
